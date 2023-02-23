1d1e1bdb725d20d8d596b44829a503318a597986
truonghieu15 <hieuhop2003@gmail.com>
2023-02-23 14:24:24 +0700

    ass

--- pages/about.js
+++ pages/about.js
@@ -74,7 +74,7 @@ ${header()}
   <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
   ${data.map((project)=>{
     return`    
-    <div class="git_hub w-[350px] ml-10 bg-white hover:mt-[15px]  hover:bg-[#498137] ">
+    <div class="git_hub w-[350px] ml-10 bg-white hover:opacity-80 ">
       <img src="${project.img}" alt="">
       <h5 class="text-center text-xl text-[#161212] font-[600]">${project.name}</h5>
       <p class="text-[#161212] w-[300px] ml-7">${project.text} </p>
